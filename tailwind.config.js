b04c126d849a0a76edb6df84cd3a16dc5a11bbc1
narmeeniqbal10 <narmeen.miqbal10@gmail.com>
2022-04-09 13:43:43 +0500

    ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
       backgroundImage: theme => ({
         'top-bg': "url('../imgs/topbg.jpg')",
         'hero-bg': "url('../imgs/herobg.jpg')",
+        'hero-bg2': "url('../imgs/herobg2.png')",
+        'hero-bgwaves': "url('../imgs/hero-waves.png')",
         'cost-bg': "url('../imgs/costbg.svg')",
         'getStart-bg': "url('../imgs/justClickbg.svg')",
         'girlimg': "url('../imgs/tm-girl.jpg')",
